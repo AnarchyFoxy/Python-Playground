15a8fb335538591b7e825ac45bdb43ba41fb47a2
Astryda Malinowska <astamalinowska@outlook.com>
2022-12-14 22:49:31 +0100

    having fun with strings

---
+++ block_string.py
@@ -0,0 +1,24 @@
+#!/usr/share/env python3
+
+sith_code = """
+Peace is a lie. There is only Passion.
+Through Passion, I gain Strength.
+Through Strength, I gain Power.
+Through Power, I gain Victory.
+Through Victory my chains are Broken.
+The Force shall free me.
+"""
+
+print(sith_code)
+#output as expected by block string
+
+code_of_the_sith = (
+    "Peace is a lie. There is only Passion.\n"
+    "Through Passion, I gain Strength.\n"
+    "Through Strength, I gain Power.\n"
+    "Through Power, I gain Victory.\n"
+    "Through Victory my chains are Broken.\n"
+    "The Force shall free me.\n"
+)
+
+print(code_of_the_sith)
---
+++ strings_in_action.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+print(len("abc"))
+print("abc" + "def")
+print("Ni!" * 5)
+print("-" * 80)
+'''
+3
+abcdef
+Ni!Ni!Ni!Ni!Ni!
+--------------------------------------------------------------------------------
+'''
+
+myjob = "hacker"
+for c in myjob: print(c, end=' ')
+#h a c k e r 
+print()
+print('k' in myjob)
+print('z' in myjob)
+print('jajko' in "asefwefewjajkoadsgqwdfgqy")
