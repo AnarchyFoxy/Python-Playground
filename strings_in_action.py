f0f3eaff7526ac8c30908fd75b75ed076d6bbb88
Astryda Malinowska <astamalinowska@outlook.com>
2023-01-12 21:22:09 +0100

    having fun with strings

--- strings_in_action.py
+++ strings_in_action.py
@@ -18,3 +18,35 @@ print()
 print('k' in myjob)
 print('z' in myjob)
 print('jajko' in "asefwefewjajkoadsgqwdfgqy")
+"""
+True
+False
+True
+"""
+
+S = "abcdefghijklmnopqrstuwxyz"
+print(S[1:24:2])
+print(S[::2])
+""""
+bdfhjlnprtwy
+acegikmoqsuxz
+"""
+
+S = "halo"
+print(S[::-1])
+#olah
+
+S = "abcdefg"
+print(S[5:1:-1])
+#fedc
+
+print("Mielonka"[1:3])
+print("Mielonka"[slice(1, 3)])
+print("Mielonka"[::-1])
+print("Mielonka"[slice(None, None, -1)])
+"""
+ie
+ie
+aknoleiM
+aknoleiM
+"""
